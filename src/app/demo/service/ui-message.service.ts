23592af507d8495e6b4dacbaa38c7272cd8b0eea
t1vr <tanviramd32@gmail.com>
2024-05-26 03:54:34 +0600

    add message service

---
+++ src/app/demo/service/ui-message.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UiMessageService {
+
+  private currentCurriculumId = new BehaviorSubject<number>(null);
+
+  sendCurrentCurriculumId(currentCurriculumId: number) {
+    this.currentCurriculumId.next(Number(currentCurriculumId));
+  }
+
+  getCurrentCurriculumId(): Observable<number> {
+     this.currentCurriculumId.asObservable().subscribe(x=>{
+      console.log(x)
+     });
+     return this.currentCurriculumId.asObservable();
+  }
+
+}
